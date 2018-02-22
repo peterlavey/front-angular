a242a2a3849b3a454d23c3d1224910bb6c493910
Pedro Cisternas <eduardo.rodriguez@scotiabank.cl>
2018-02-22 17:51:12 -0300

    Fix rutas en variables pendiente...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { ExampleDirective } from './example.directive';
 import { ExampleComponent } from './example/example.component';
 import { ChildComponent } from './child/child.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule} from '@angular/material';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleDirective,
     ExampleComponent,
     ChildComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    //NoopAnimationsModule,
     MatInputModule
   ],
   exports: [
