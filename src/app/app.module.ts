5bb249f1c14a39f1e056c26f7d21e953af10c88b
Pedro Cisternas <eduardo.rodriguez@scotiabank.cl>
2018-02-22 16:44:35 -0300

    Integración con angular material. Ejemplos de componente padre e hijo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ExampleDirective } from './example.directive';
+import { ExampleComponent } from './example/example.component';
+import { ChildComponent } from './child/child.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule} from '@angular/material';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleDirective,
+    ExampleComponent,
+    ChildComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    //NoopAnimationsModule,
+    MatInputModule
+  ],
+  exports: [
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
