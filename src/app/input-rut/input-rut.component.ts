86883a4dbf0a53c0a5dd692e92f6f6097550a6af
Pedro Cisternas <eduardo.rodriguez@scotiabank.cl>
2018-02-26 15:52:20 -0300

    Component RUT completo

--- src/app/input-rut/input-rut.component.ts
+++ src/app/input-rut/input-rut.component.ts
@@ -1,27 +1,76 @@
-import {Component, Input, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-input-rut',
   templateUrl: './input-rut.component.html',
   styleUrls: ['./input-rut.component.css']
 })
-export class InputRutComponent implements OnChanges {
+export class InputRutComponent {
   private _rut: string;
+  public isValid: boolean;
 
+  @Output() state = new EventEmitter<boolean>();
   @Input() set rut(rut: string) {
-    this._rut = rut;
+    if (rut) {
+      const lastCharacter = (event as any).data;
+      if (typeof lastCharacter === 'string') {
+        if (!!lastCharacter.match(/([0-9]|[kK])/)) {
+          if (rut.length > 7) {
+            this._rut = this.formateaRut(rut);
+          } else {
+            this.isValid = true;
+            this.state.emit(false);
+          }
+        } else {
+          (event as any).currentTarget.value = rut.substring(0, rut.length - 1);
+        }
+      }
+    }
   }
 
   get rut(): string {
-    return this._rut.toUpperCase();
+    return this._rut;
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    const rut: SimpleChange = changes.rut;
-    this._rut = rut.currentValue.toUpperCase();
+  validaDV(T) {
+    let M = 0;
+    let S = 1;
+    for (; T; T = Math.floor(T / 10)) {
+      S = (S + T % 10 * (9 - M ++ % 6)) % 11;
+    }
+    return (S ? S - 1 : 'k').toString();
+  }
+
+  formateaRut(rut) {
+    let rutPuntos = '';
+    let dv = '';
+    const actual = rut.replace(/^0+/, '');
+    if (actual !== '' && actual.length > 1) {
+      const sinPuntos = actual.replace(/\./g, '');
+      const actualLimpio = sinPuntos.replace(/-/g, '');
+      const inicio = actualLimpio.substring(0, actualLimpio.length - 1);
+      let i = 0;
+      let j = 1;
+      for (i = inicio.length - 1; i >= 0; i--) {
+        const letra = inicio.charAt(i);
+        rutPuntos = letra + rutPuntos;
+        if (j % 3 === 0 && j <= inicio.length - 1) {
+          rutPuntos = '.' + rutPuntos;
+        }
+        j++;
+      }
+      dv = actualLimpio.substring(actualLimpio.length - 1);
+    }
+    if (dv === 'K') {
+      dv = 'k' ;
+    }
+    this.isValid = this.validaDV(parseInt(rutPuntos.replace(/\./g, ''), 10)) === dv;
+    this.state.emit(this.isValid);
+    return `${rutPuntos}-${dv}`;
   }
 
   constructor() {
+    this.isValid = true;
   }
 
 }
